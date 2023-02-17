7714295051e54d24c5269648f6655ee86332d2bb
phat <nguyenvanphat30499@gmail.com>
2023-02-17 07:19:42 +0700

    adnew_domain

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000','https://nguyen-phat-graduation-internship-project.surge.sh/'],
+    origin: ['https://nguyen-phat-graduation-internship-project.surge.sh'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
